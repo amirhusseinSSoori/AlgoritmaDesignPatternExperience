9a57509a4fa1ad9e5342b5ddf8ac1bc188c870e7
amirss <amirhusseinsoori75@gmail.com>
2023-01-01 14:32:10 +0330

    add information for Sqrt

--- app/src/main/java/com/example/algoritmaexperiencekotlin/repository/Sqrt.kt
+++ app/src/main/java/com/example/algoritmaexperiencekotlin/repository/Sqrt.kt
@@ -1,5 +1,35 @@
 package com.example.algoritmaexperiencekotlin.repository
 
+/**
+Sqrt(x)
+
+Topic : Binary Search ,Math
+
+Companies : Microsoft , Google ,Linkedin , Amazon
+
+Implement in sqrt(int x).
+
+Compute and return the square root of x, where x
+is guaranteed to be a non-negative integer.
+
+Since the return type is an integer , the decimal
+digits integer part of the result is returned.
+
+Example 1:
+Input:4
+Output:2
+
+
+Example 2:
+Input:8
+Output:2
+
+Explanation:
+The square root of 8 is 2.82842.., and since the decimal part is truncated, 2 is returned.
+
+
+
+ */
 class Sqrt {
     fun mySqrt(x: Int): Int {
         if (x <= 1) {
@@ -20,7 +50,4 @@ class Sqrt {
         }
         return hi
     }
-}
-fun main(){
-    print(17/2)
 }
